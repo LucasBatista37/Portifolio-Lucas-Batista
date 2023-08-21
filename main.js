c8a58c4eeb6002b0e07e521e9718ce9fdd5c3c74
Lucas Batista <124750301+LucasBatista37@users.noreply.github.com>
2023-08-21 09:48:13 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,63 @@
+function addColorClass(event) {
+  const skill = event.currentTarget;
+  const icon = skill.querySelector('i');
+  const skillName = skill.querySelector('.skill-name');
+
+  if (skill.classList.contains('color-html')) {
+    icon.style.color = '#E44D26'; 
+  } else if (skill.classList.contains('color-css')) {
+    icon.style.color = '#2965f1'; 
+  } else if (skill.classList.contains('color-js')) {
+    icon.style.color = '#F0DB4F'; 
+  } else if (skill.classList.contains('color-node')) {
+    icon.style.color = '#689f63'; 
+  } else if (skill.classList.contains('color-php')) {
+    icon.style.color = '#777bb4';
+  } else if (skill.classList.contains('color-mysql')) {
+    icon.style.color = '#4479A1';
+  } else if (skill.classList.contains('color-azure')) {
+    icon.style.color = '#0089D6'; 
+  } else if (skill.classList.contains('color-figma')) {
+    icon.style.color = '#F24E1E'; 
+  } else if (skill.classList.contains('color-git')) {
+    icon.style.color = '#F05032'; 
+  }
+  
+  skillName.style.color = '#1d1d1d'; 
+}
+
+function removeColorClass(event) {
+  const skill = event.currentTarget;
+  const icon = skill.querySelector('i');
+  const skillName = skill.querySelector('.skill-name');
+
+  icon.style.color = '#000'; 
+  skillName.style.color = '#555'; 
+}
+
+
+const skills = document.querySelectorAll('.skill');
+
+skills.forEach(skill => {
+  skill.addEventListener('mouseenter', addColorClass);
+  skill.addEventListener('mouseleave', removeColorClass);
+});
+
+// Seu JavaScript existente...
+
+// Função para manipular a exibição do menu responsivo
+function toggleMenu() {
+  var menuOverlay = document.querySelector('.menu-overlay');
+  menuOverlay.style.display = menuOverlay.style.display === 'block' ? 'none' : 'block';
+}
+
+// Evento de clique no ícone de menu para mostrar o menu
+var menuIcon = document.querySelector('.fa-bars');
+menuIcon.addEventListener('click', toggleMenu);
+
+// Evento de clique nos links do menu para ocultar o menu quando um link é clicado (opcional)
+var navLinks = document.querySelectorAll('.nav-links li a');
+navLinks.forEach(link => {
+  link.addEventListener('click', toggleMenu);
+});
+
